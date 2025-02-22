e07af079fd58588c93a44997383d13099fe3c3dc
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-02-22 23:33:36 +0800

    fix: update setup.py, clean custom class

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 from setuptools import setup, find_packages
-from setuptools.command.bdist_wheel import bdist_wheel as _bdist_wheel
 import os
-
-class bdist_wheel(_bdist_wheel):
-    def finalize_options(self):
-        '''强制设置程序包和平台相关'''
-        _bdist_wheel.finalize_options(self)
-        self.root_is_pure = False
-
-    def get_tag(self):
-        '''强制设置tag，否则默认程序包和python版本相关'''
-        impl, abi_tag, plat_name = _bdist_wheel.get_tag(self)
-        impl = 'py3'
-        abi_tag = 'none'
-        return impl, abi_tag, plat_name
 
 
 # 读取版本号
@@ -45,9 +31,7 @@ setup(
     packages=find_packages(),
     package_data={'pygrt': ['./C_extension/*']},
     include_package_data=True,
-    cmdclass={
-        'bdist_wheel': bdist_wheel
-    },
+
     install_requires=[
         'numpy>=1.20, <2.0',
         'scipy>=1.10',
