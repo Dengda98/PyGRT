6da2b3cf8fe4b056216b93ae904d326e80e0629b
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-04-15 17:57:59 +0800

    REFAC: `from ctypes import *` out of use, and replace `|` with `Union`

--- pygrt/c_structures.py
+++ pygrt/c_structures.py
@@ -13,6 +13,17 @@
 from ctypes import *
 from ctypes.wintypes import PFLOAT
 
+__all__ = [
+    "USE_FLOAT",
+    "NPCT_REAL_TYPE",
+    "NPCT_CMPLX_TYPE",
+
+    "REAL",
+    "PREAL",
+
+    "c_PyModel1D",
+    "c_GRN",
+]
 
 
 USE_FLOAT = False
