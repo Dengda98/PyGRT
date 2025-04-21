9755faad8f339dd7697e1cf770265a3c41869761
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-04-21 13:26:37 +0800

    DOC: update `mod1` in `example/multi_traces`

--- example/multi_traces/plot.py
+++ example/multi_traces/plot.py
@@ -23,4 +23,4 @@ ax.set_xlim([0.5, 1.5])
 ax.set_ylim([0, 15])
 ax.yaxis.set_inverted(True)
 
-fig.savefig("multi_traces.png", bbox_inches='tight', dpi=200)
+fig.savefig("multi_traces.png", bbox_inches='tight', dpi=100)
