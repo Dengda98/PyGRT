ce69f625dc29a4d24d217810f3ba9116d739b879
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-08-03 11:08:23 +0800

    STYLE: grt_k2a.c

--- pygrt/C_extension/src/common/grt_k2a.c
+++ pygrt/C_extension/src/common/grt_k2a.c
@@ -8,63 +8,40 @@
  * 
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #include "common/const.h"
-#include "common/logo.h"
-#include "common/colorstr.h"
 #include "common/iostats.h"
+#include "common/util.h"
+
+#include "grt.h"
+
+/** 该子模块的参数控制结构体 */
+typedef struct {
+    char *name;
+    /** 输入模型 */
+    char *s_filepath;
+} GRT_SUBMODULE_CTRL;
 
 
-//****************** 在该文件以内的全局变量 ***********************//
-// 命令名称
-static char *command = NULL;
+/** 释放结构体的内存 */
+static void free_Ctrl(GRT_SUBMODULE_CTRL *Ctrl){
+    free(Ctrl->s_filepath);
+    free(Ctrl);
+}
 
 /**
  * 打印使用说明
  */
 static void print_help(){
-print_logo();
 printf("\n"
-"[grt.k2a]\n\n"
+"[grt k2a]\n\n"
 "    Convert a binary stats file generated during wavenumber integration\n"
 "    into an ASCII file, write to standard output.\n"
 "\n\n"
 "Usage:\n"
 "----------------------------------------------------------------\n"
-"    grt.k2a <statsfile>\n"
+"    grt k2a <statsfile>\n"
 "\n\n\n"
 );
-}
-
-
-/**
- * 从路径字符串中找到用/或\\分隔的最后一项
- * 
- * @param    path     路径字符串指针
- * 
- * @return   指向最后一项字符串的指针
- */
-static const char* get_basename(const char* path) {
-    // 找到最后一个 '/'
-    char* last_slash = strrchr(path, '/'); 
-    
-#ifdef _WIN32
-    char* last_backslash = strrchr(path, '\\');
-    if (last_backslash && (!last_slash || last_backslash > last_slash)) {
-        last_slash = last_backslash;
-    }
-#endif
-    if (last_slash) {
-        // 返回最后一个 '/' 之后的部分
-        return last_slash + 1; 
-    }
-    // 如果没有 '/'，整个路径就是最后一项
-    return path; 
 }
 
 
@@ -74,37 +51,16 @@ static const char* get_basename(const char* path) {
  * @param     argc      命令行的参数个数
  * @param     argv      多个参数字符串指针
  */
-static void getopt_from_command(int argc, char **argv){
+static void getopt_from_command(GRT_SUBMODULE_CTRL *Ctrl, int argc, char **argv){
     int opt;
     while ((opt = getopt(argc, argv, ":h")) != -1) {
         switch (opt) {
-
-            // 帮助
-            case 'h':
-                print_help();
-                exit(EXIT_SUCCESS);
-                break;
-
-            // 参数缺失
-            case ':':
-                fprintf(stderr, "[%s] " BOLD_RED "Error! Option '-%c' requires an argument. Use '-h' for help.\n" DEFAULT_RESTORE, command, optopt);
-                exit(EXIT_FAILURE);
-                break;
-
-            // 非法选项
-            case '?':
-            default:
-                fprintf(stderr, "[%s] " BOLD_RED "Error! Option '-%c' is invalid. Use '-h' for help.\n" DEFAULT_RESTORE, command, optopt);
-                exit(EXIT_FAILURE);
-                break;
+            GRT_Common_Options_in_Switch(Ctrl, (char)(optopt));
         }
     }
 
     // 检查必选项有没有设置
-    if(argc != 2){
-        fprintf(stderr, "[%s] " BOLD_RED "Error! Need set options. Use '-h' for help.\n" DEFAULT_RESTORE, command);
-        exit(EXIT_FAILURE);
-    }
+    GRTCheckOptionEmpty(Ctrl, argc != 2);
 }
 
 
@@ -147,43 +103,40 @@ static void print_PTAM(FILE *fp){
 
 
 int k2a_main(int argc, char **argv){
-    command = argv[0];
+    GRT_SUBMODULE_CTRL *Ctrl = calloc(1, sizeof(*Ctrl));
+    Ctrl->name = argv[0];
 
-    getopt_from_command(argc, argv);
+    getopt_from_command(Ctrl, argc, argv);
 
-    const char *filepath = argv[1];
+    Ctrl->s_filepath = argv[1];
     // 检查文件名是否存在
-    if(access(filepath, F_OK) == -1){
-        fprintf(stderr, "[%s] " BOLD_RED "Error! %s not exists.\n" DEFAULT_RESTORE, command, filepath);
-        exit(EXIT_FAILURE);
+    if(access(Ctrl->s_filepath, F_OK) == -1){
+        GRTRaiseError("[%s] Error! %s not exists.\n", Ctrl->name, Ctrl->s_filepath);
     }
 
 
     // 打开stats
     FILE *fp=NULL;
-    if((fp = fopen(filepath, "rb")) == NULL){
-        fprintf(stderr, "[%s] " BOLD_RED "Error! Can't read %s.\n" DEFAULT_RESTORE, command, filepath);
-        exit(EXIT_FAILURE);
+    if((fp = fopen(Ctrl->s_filepath, "rb")) == NULL){
+        GRTRaiseError("[%s] Error! Can't read %s.\n", Ctrl->name, Ctrl->s_filepath);
     }
 
     // 根据文件名确定函数
-    const char *basename = get_basename(filepath);
+    const char *basename = get_basename(Ctrl->s_filepath);
     if(strncmp(basename, "PTAM", 4) == 0) {
         print_PTAM(fp);
     } else if(strncmp(basename, "K", 1) == 0) {
         print_K(fp);
     } else {
-        fprintf(stderr, "[%s] " BOLD_RED "Error! Can't read %s.\n" DEFAULT_RESTORE, command, filepath);
-        exit(EXIT_FAILURE);
+        GRTRaiseError("[%s] Error! Can't read %s.\n", Ctrl->name, Ctrl->s_filepath);
     }
 
     // 检查是否是因为文件结束而退出
     if (ferror(fp)) {
-        fprintf(stderr, "[%s] " BOLD_RED "Error reading file %s.\n" DEFAULT_RESTORE, command, filepath);
-        exit(EXIT_FAILURE);
+        GRTRaiseError("[%s] Error reading file %s.\n", Ctrl->name, Ctrl->s_filepath);
     }
-
     fclose(fp);
 
+    free_Ctrl(Ctrl);
     return EXIT_SUCCESS;
 }
