6315855e76ae7f1804a431363c3adee88d10ad61
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-09-17 19:29:16 +0800

    update shell scripts

--- example/far_field/run.sh
+++ example/far_field/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grt greenfn -Mmilrow -OGRN -N1400/1 -D10/0 -R1800 -La20/1e-2/0 -E100  # -S-1  -G0/1/1/0
+grt greenfn -Mmilrow -OGRN -N1400/1 -D10/0 -R1800 -La20/1e-2/0 -E100  # -S-1  -Gvh
