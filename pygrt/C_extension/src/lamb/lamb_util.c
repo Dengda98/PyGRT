0b6ef861a9ecb304bcd3dc42b23f6b33195c9285
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-11-24 23:09:16 +0800

    FEAT: in `lamb1`, add two functions to solve two-type Rayleigh equations

--- pygrt/C_extension/src/lamb/lamb_util.c
+++ pygrt/C_extension/src/lamb/lamb_util.c
@@ -8,7 +8,16 @@
 
 #include "grt/lamb/lamb_util.h"
 
-void grt_roots3(real_t a, real_t b, real_t c, cplx_t y3[3])
+
+/**
+ * 求解一元三次方程的根， \f$ x^3 + ax^2 + bx + c = 0 \f$
+ * 
+ * @param[in]      a     系数 a
+ * @param[in]      b     系数 b
+ * @param[in]      c     系数 c
+ * @param[out]     y3    三个根，其中 y3[2] 为正根
+ */
+static void _roots3(real_t a, real_t b, real_t c, cplx_t y3[3])
 {
     real_t Q, R;
     Q = (a*a - 3.0*b) / 9.0;
@@ -35,6 +44,31 @@ void grt_roots3(real_t a, real_t b, real_t c, cplx_t y3[3])
         y3[1] = - 0.5 * (A+B) -  a/3.0 - I*sqrt(3.0)/2.0*(A - B);
         y3[2] = A + B - a/3.0;
     }
+}
+
+void grt_rayleigh1_roots(real_t nu, cplx_t y3[3])
+{
+    real_t a, b, c;
+    real_t nu2, nu3, nu4;
+    nu2 = nu*nu;
+    nu3 = nu2*nu;
+    nu4 = nu3*nu;
+    real_t snu = 1.0 - nu;
+    real_t snu2 = snu*snu;
+    real_t snu3 = snu2*snu;
+    a = -0.5 * (2.0*nu2 + 1.0)/snu;
+    b = 0.25 * (4.0*nu3 - 4.0*nu2 + 4.0*nu - 1.0)/snu2;
+    c = -0.125*nu4/snu3;
+    _roots3(a, b, c, y3);
+}
+
+void grt_rayleigh2_roots(real_t m, cplx_t y3[3])
+{
+    real_t a, b, c;
+    a = 0.5*(2.0*m - 3.0)/(1.0 - m);
+    b = 0.5/(1.0 - m);
+    c = - 0.0625/(1 - m);
+    _roots3(a, b, c, y3);
 }
 
 cplx_t grt_evalpoly2(const cplx_t *C, const int n, const cplx_t y, const int offset)
