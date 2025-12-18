6d8900025c8a2c9b46ecaee7ff582d64cc3ddef0
Dengda Zhu <zhudengda21@mails.ucas.ac.cn>
2025-12-18 17:00:08 +0800

    FIX: keep receiver upward when source and receiver are at the same depth (#188)

--- test/compare_sac.py
+++ test/compare_sac.py
@@ -10,6 +10,9 @@ sacpath2 = sys.argv[2]
 st1 = read(sacpath1)
 st2 = read(sacpath2)
 
+tol = 0.01
+errLst = []
+
 for tr1 in st1:
     ch = tr1.stats.channel
     tr2 = st2.select(channel=ch)[0]
@@ -24,11 +27,19 @@ for tr1 in st1:
 
     # All zeros
     if np.all(data1==0.0) and np.all(data2==0.0):
+        errLst.append(0.0)
         continue
     
-    tol = 0.01
     err = np.sum(np.abs(data2 - data1))/np.mean(np.abs(data1))
-    if err > tol:
-        raise ValueError(f"Error({err}) > {tol} in {tr1} and {tr2} from {sacpath1} and {sacpath2}!!")
-    
-        
+    errLst.append(err)
+    if np.any(err > tol):
+        print(f"Error({err}) > {tol} in {tr1} and {tr2} from {sacpath1} and {sacpath2}!!")
+
+errLst = np.array(errLst)
+
+print('-'*100)
+print(sacpath1, sacpath2)
+print(errLst)
+print('-'*100)
+if np.any(errLst > tol):
+    raise ValueError(f"Error!!")
