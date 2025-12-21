2c7ae434887bde37acf98ee9a0d7ce155fd3a39a
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-12-21 12:54:33 +0800

    fix ex06

--- docs/source/Gallery/ex06/run.sh
+++ docs/source/Gallery/ex06/run.sh
@@ -10,7 +10,7 @@ grt greenfn -Mmilrow -OGRN_SAFIM -N1400/1 -D2/0 -R1800 -L+a1e-2 -E100
 
 python plot.py
 
-mv safim.svg cover.svg
+cp safim.svg cover.svg
 
 ex=$(basename $(pwd))
 cd .. && tar -czvf ${ex}.tar.gz ${ex} && mv ${ex}.tar.gz ${ex} && cd -
