b2231934fd409a5b80a9a14bf45c5a280c02c192
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-12-21 12:52:30 +0800

    remove outdated/duplicated examples

--- example/kernel_freq_response/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-grt greenfn -Mmod1 -D0.01/0 -N500/0.02/0.8 -OGRN -R1 -K+v0.1 -S -L20
--- example/view_integ_stats/run_stgrt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-
-grt static greenfn -Mhalfspace2 -D0.1/0 -X-3/3/0.6 -Y-5/5/0.5 -Ostgrn.nc -S
