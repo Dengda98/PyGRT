9f5bbaddcc3bf3519cb3c3f5c78a81f0cb8d9357
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2025-12-21 22:28:16 +0800

    update

--- docs/source/Gallery/ex02/run.sh
+++ docs/source/Gallery/ex02/run.sh
@@ -14,9 +14,9 @@ out="GRN"
 
 diff=0.001
 deprcv=3.2
-deprcv2=`echo $deprcv+$diff | bc | awk '{printf("%.3f", $1)}'`
+deprcv2=$(echo $deprcv+$diff | bc | awk '{printf("%.3f", $1)}')
 dist=10
-dist2=`echo $dist+$diff | bc | awk '{printf("%.3f", $1)}'`
+dist2=$(echo $dist+$diff | bc | awk '{printf("%.3f", $1)}')
 
 grt greenfn -M${modname} -O${out} -N${nt}/${dt} -D${depsrc}/$deprcv -R$dist -e
 grt greenfn -M${modname} -O${out} -N${nt}/${dt} -D${depsrc}/$deprcv2 -R$dist
