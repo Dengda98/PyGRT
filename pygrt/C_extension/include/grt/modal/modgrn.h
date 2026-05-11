4a1f38315c15691a171377a3afefc094be1d5c46
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2026-05-11 22:17:17 +0800

    update modgrn.h/c

--- pygrt/C_extension/include/grt/modal/modgrn.h
+++ pygrt/C_extension/include/grt/modal/modgrn.h
@@ -15,5 +15,12 @@
 #include "grt/modal/eigenfn.h"
 
 
-
+/**
+ * 根据频散结果，利用模态叠加法计算不同震源激发的面波格林函数
+ * 
+ * @param[in,out]      mod1d       模型结构体指针
+ * @param[in]          wtype       频散类型
+ * @param[in,out]      eigfnmet    本征函数数据结构体指针
+ * @param[out]         grn         格林函数频谱
+ */
 void grt_modsum_grn_spec(MODEL1D *mod1d, const DISPER_TYPE wtype, EIGENFN_INFO *eigfnmet, GRNSPEC *grn);
