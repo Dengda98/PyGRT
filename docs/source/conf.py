c21b88d15fcec9a3929aac7db0d22496a98690e4
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2026-05-20 10:24:08 +0800

    update xie1989

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -160,7 +160,8 @@ rst_epilog = f"""
 .. |bouchon1981p| replace:: :ref:`Bouchon (1981) <reference>`
 .. |ji1995| replace:: :ref:`纪晨和姚振兴, 1995 <reference>`
 .. |ji1995p| replace:: :ref:`纪晨和姚振兴 (1995) <reference>`
-.. |xie1989| replace:: :ref:`(谢小碧和姚振兴, 1989) <reference>`
+.. |xie1989| replace:: :ref:`谢小碧和姚振兴, 1989 <reference>`
+.. |xie1989p| replace:: :ref:`谢小碧和姚振兴 (1989) <reference>`
 .. |yao2026| replace:: :ref:`(姚振兴和谢小碧, 2026) <reference>`
 .. |yao1983| replace:: :ref:`(Yao and Harkrider, 1983) <reference>`
 .. |zhang2021| replace:: :ref:`(张海明, 2021) <reference>`
