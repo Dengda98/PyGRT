4861d288b51ef099d0034d35c001593d5d6e77c7
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2026-05-20 10:42:59 +0800

    update zhang2003

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -172,7 +172,8 @@ rst_epilog = f"""
 .. |chen2001p| replace:: :ref:`Chen and Zhang (2001) <reference>`
 .. |zhang2024| replace:: :ref:`张海明和冯禧, 2024 <reference>`
 .. |zhang2024p| replace:: :ref:`张海明和冯禧 (2024) <reference>`
-.. |zhang2003| replace:: :ref:`(Zhang et al., 2003) <reference>`
+.. |zhang2003| replace:: :ref:`Zhang et al., 2003 <reference>`
+.. |zhang2003p| replace:: :ref:`Zhang et al. (2003) <reference>`
 .. |kennett1979| replace:: :ref:`(Kennett and Kerry, 1979) <reference>`
 .. |pygrt2025| replace:: :ref:`(Zhu et al., 2025a) <reference>`
 .. |dcm2025| replace:: :ref:`(Zhu et al., 2025b) <reference>`
