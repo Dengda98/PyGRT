a96911468dbdd8e63c73a658bc8db76fe1d68098
Dengda98 <zhudengda@mail.iggcas.ac.cn>
2026-08-26 16:41:36 +0800

    BUILD: change the installation method fo GMT

--- docs/compile.sh
+++ docs/compile.sh
@@ -9,18 +9,27 @@ cd ../pygrt/C_extension
 make clean && make -j
 cd -
 
-# 添加到环境变量
-# cp ../pygrt/C_extension/bin/* /usr/local/bin/
-# export PATH=$(realpath ../pygrt/C_extension/bin):$PATH
-# echo "export PATH=$(realpath ../pygrt/C_extension/bin):$PATH" >> ~/.bashrc
-cp ../pygrt/C_extension/bin/* ${READTHEDOCS_VIRTUALENV_PATH}/bin/
+# 将编译出的 grt 放入当前构建环境的 bin 目录
+grt_source_dir="$(realpath ../pygrt/C_extension/bin)"
+if [[ -n "${READTHEDOCS_VIRTUALENV_PATH:-}" ]]; then
+    grt_bin_dir="${READTHEDOCS_VIRTUALENV_PATH}/bin"
+elif [[ -n "${CONDA_PREFIX:-}" ]]; then
+    grt_bin_dir="${CONDA_PREFIX}/bin"
+else
+    grt_bin_dir="${grt_source_dir}"
+fi
+
+if [[ "${grt_bin_dir}" != "${grt_source_dir}" ]]; then
+    cp "${grt_source_dir}"/* "${grt_bin_dir}/"
+fi
+export PATH="${grt_bin_dir}:${PATH}"
 echo "-------------------------"
-echo $PATH
+echo "${PATH}"
 # echo "-------------------------"
 # echo $(ls /usr/local/bin/* -l)
 echo "-------------------------"
-echo ${READTHEDOCS_VIRTUALENV_PATH}
-echo ${READTHEDOCS_REPOSITORY_PATH}
+echo "${grt_bin_dir}"
+echo "${READTHEDOCS_REPOSITORY_PATH:-}"
 # if [[ $(which grt) == "" ]]; then
 # echo "export PATH=$(realpath ../pygrt/C_extension/bin):\$PATH" >> ~/.bashrc
 # source ~/.bashrc
